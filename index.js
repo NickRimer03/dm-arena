5c81b5715e9c47eb96d959ec637258c818bc2b1f
Nick Rimer <nickrimer.03@gmail.com>
2019-10-24 23:15:18 +0300

    -- spaceship flying

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import Phaser from "phaser";
 
-const { AUTO, Game, Math } = Phaser;
+const { AUTO, Game } = Phaser;
+const [width, height] = [800, 600];
 
 const config = {
   type: AUTO,
-  width: 800,
-  height: 600,
+  width,
+  height,
+  physics: {
+    default: "arcade",
+    arcade: {
+      fps: 60,
+      gravity: { y: 0 },
+      debug: true
+    }
+  },
   scene: {
     preload: preload,
     create: create,
@@ -15,19 +24,32 @@ const config = {
 
 const game = new Game(config);
 let player;
+let cursors;
 
 function preload() {
   this.load.image("sky", "./res/img/sky.png");
-  this.load.atlas(
-    "vulture",
-    "./res/spritesheets/vulture.png",
-    "./res/spritesheets/vulture.json"
-  );
+  this.load.atlas("vulture", "./res/spritesheets/vulture.png", "./res/spritesheets/vulture.json");
 }
 
 function create() {
-  this.add.image(400, 300, "sky");
-  player = this.add.sprite(100, 450, "vulture");
+  this.add.image(width / 2, height / 2, "sky");
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  player = this.physics.add.image(100, 100, "vulture");
+  player.setDamping(true);
+  player.setDrag(0.975);
+  player.setMaxVelocity(200);
 }
 
-function update() {}
+function update() {
+  if (cursors.up.isDown) {
+    if (cursors.left.isDown) {
+      player.angle -= 2;
+    } else if (cursors.right.isDown) {
+      player.angle += 2;
+    }
+
+    this.physics.velocityFromRotation(player.rotation, 200, player.body.velocity);
+  }
+}
